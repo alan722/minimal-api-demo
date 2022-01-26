6c7a566545a549e644b84be25bfea01c98e3def5
Alan Chen <alan72291@live.com>
2022-01-26 19:41:03 +0200

    initial commit

---
+++ UserDB/dbo/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+	@Id int
+AS
+BEGIN
+	SELECT Id, FirstName, LastName, Token 
+	FROM dbo.[User]
+	WHERE Id = @Id;
+END
---
+++ UserDB/dbo/StoredProcedures/spUser_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Token nvarchar(50)
+AS
+BEGIN
+	INSERT INTO dbo.[User] (FirstName, LastName, Token)
+	VALUES (@FirstName, @LastName, @Token)
+END
